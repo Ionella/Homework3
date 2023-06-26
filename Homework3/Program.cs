417fd53c8966fadd19fa87a802fc137cf8c32f9d
Ionella <137092984+Ionella@users.noreply.github.com>
2023-06-26 20:39:58 +0300

    Update Program.cs

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -1,40 +1,39 @@
 ﻿using System;
-using System.Threading.Tasks.Dataflow;
 
-namespace homework3
+namespace Homework3
 {
-    public class myClasa
+    public class MyClasa
     {
-        public void afiseazaNumar(int Numar)
+        public void AfiseazaNumar(int numar)
         {
-            Console.WriteLine("Numarul este " + Numar);
-            if (Numar > 0)
+            Console.WriteLine("Numarul este " + numar);
+            if (numar > 0)
                 Console.WriteLine("Numarul este pozitiv.");
             else
                 Console.WriteLine("Numarul este negativ.");
         }
-        /// ============================================================
+        
         public void CalculeazaSuma(int a, int b)
         {
             int suma = a + b;
             Console.WriteLine("Suma numerelor este:" + suma);
         }
-        /// ============================================================
+        
         public static void CalculeazaProdus(int a, int b)
         {
             int produs = a * b;
             Console.WriteLine("Produsul numerelor este:" + produs);
         }
-        /// ============================================================
+        
         }
     class Program
     {
         static void Main(string[] args)
         {
-            myClasa obj = new myClasa();
-            obj.afiseazaNumar(-10);
+            MyClasa obj = new MyClasa();
+            obj.AfiseazaNumar(-10);
             obj.CalculeazaSuma(-5, 3);
-            myClasa.CalculeazaProdus(2, -4);
+            MyClasa.CalculeazaProdus(2, -4);
          
         }
     }
